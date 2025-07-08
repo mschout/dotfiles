6a00a4a20705552206608eaeca621e5278bef717
Michael Schout <schoutm@gmail.com>
2025-07-08 15:38:54 -0500

    Fix perl-build boostrap

--- .config/yadm/bootstrap.d/plenv-perl-build.sh
+++ .config/yadm/bootstrap.d/plenv-perl-build.sh
@@ -3,7 +3,10 @@
 # install the plenv perl-build plugin if necessary
 #
 
-[ -d "$HOME/.plenv/plugins" ] || exit
+set -xe
+
+# Ensure plenv is present
+[ -e "$HOME/.plenv/bin/plenv" ] || exit 0
 
 if [ ! -e "$HOME/.plenv/plugins/perl-build/bin/perl-build" ]; then
   echo "Installing plenv perl-build plugin"
